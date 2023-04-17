e13f3d615b7c879f70d20620715205ec9827dfac
David da Cunha <91962586+davidacunha@users.noreply.github.com>
2023-04-17 19:58:24 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,6 +1,6 @@
-def remove_duplicatas(lista):
-    nova_lista = []
-    for elemento in lista:
-        if elemento not in nova_lista:
-            nova_lista.append(elemento)
-    return nova_lista
+def remove_duplicates(input_list):
+    return list(set(input_list))
+
+input_list = [1, 2, 3, 4, 4, 5, 5]
+output_list = remove_duplicates(input_list)
+print(output_list)
